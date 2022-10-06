50a88f1fee36f5a58c464ba1ac8dcba5d13db8aa
Ambroise <ambroise@sunnypeak.net>
2022-10-06 20:19:15 +0200

    Added a sync of timer for the end of the shop phase

--- Super Auto Time/Assets/Scripts/PlayerController.cs
+++ Super Auto Time/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using TMPro;
 
 public struct Unite
 {
@@ -16,16 +17,32 @@ public struct Unite
         this.damages = damages;
     }
 }
+
+
 public class PlayerController : NetworkBehaviour
 {
-
     public readonly SyncList<Unite> board = new SyncList<Unite>();
     public List<TimeUnite> boardList;
+
+    //States of the game
+    [SyncVar]
+    public bool isBattlePhase = false;
+    [SyncVar]
+    public bool isShopPhase = true;
+    public float shopPhaseDuration = 3;
+
+    private TextMeshProUGUI timerDisplay;
+    [SyncVar]
+    public bool readyToBegin = false;
 
     // Start is called before the first frame update
     public override void OnStartClient()
     {
         board.Callback += OnBoardUpdated;
+        if (isLocalPlayer)
+        {
+            timerDisplay = GameObject.FindGameObjectWithTag("timerUI").GetComponent<TextMeshProUGUI>();
+        }
         for (int index = 0; index < board.Count; index++)
             OnBoardUpdated(SyncList<Unite>.Operation.OP_ADD, index, new Unite(), board[index]);
     }
@@ -33,29 +50,102 @@ public class PlayerController : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isLocalPlayer)
+        if (readyToBegin)
         {
-            if (Input.GetMouseButtonDown(0))
+            if (isLocalPlayer)
             {
-                foreach (TimeUnite unite in boardList)
+                if (!this.isBattlePhase)
                 {
-                    addNewUnite(unite.name, unite.health, unite.damages);
+                    StartCoroutine(shopPhase(shopPhaseDuration));
                 }
-            }
-            if (Input.GetMouseButtonDown(1))
-            {
 
-                RemoveUnite(0);
-
-            }
-            if (board.Count > 0)
-            {
-                foreach (Unite unite in board)
+                if (Input.GetMouseButtonDown(0))
                 {
-                    Debug.Log("name="+ unite.name + " health =" + unite.health.ToString()+"damages= "+unite.damages.ToString());
+                    foreach (TimeUnite unite in boardList)
+                    {
+                        addNewUnite(unite.name, unite.health, unite.damages);
+                    }
+                }
+                if (Input.GetMouseButtonDown(1))
+                {
+                    RemoveUnite(0);
+                }
+                if (board.Count > 0)
+                {
+                    foreach (Unite unite in board)
+                    {
+                        Debug.Log("name=" + unite.name + " health =" + unite.health.ToString() + "damages= " + unite.damages.ToString());
+                    }
+                }
+                if(isBattlePhase)
+                {
+                    //Switch scene et lancer le fight
                 }
             }
         }
+        if (isLocalPlayer)
+        {
+            if (isServer && shopPhaseDuration < 6)
+            {
+                shopPhaseDuration += 6;
+            }
+            if (GameObject.FindGameObjectsWithTag("Player").Length > 1)
+            {
+                setReadyToBegin();
+            }
+            if (!isBattlePhase && readyToBegin)
+            {
+                bool isReadyToBattle = true;
+                foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+                {
+                    if (player.GetComponent<PlayerController>().isShopPhase)
+                    {
+                        isReadyToBattle = false;
+                    }
+                }
+                if (isReadyToBattle)
+                {
+                    Debug.Log("GOD C'ETAIT TROP CHIANT MAIS CA MARCHE ENFIN PUTAIN");
+                    setBattlePhase(true);
+                }
+            }
+        }
+
+    }
+
+    [Command]
+    private void setReadyToBegin()
+    {
+        readyToBegin = true;
+    }
+
+    [Command]
+    private void setBattlePhase(bool newStatus)
+    {
+        isBattlePhase = newStatus;
+    }
+
+    private IEnumerator shopPhase(float shopPhaseDuration)
+    {
+        float totalTime = 0;
+        while (totalTime <= shopPhaseDuration)
+        {
+            totalTime += Time.deltaTime;
+            timerDisplay.text = (shopPhaseDuration - totalTime).ToString();
+
+            var integer = (int)totalTime; /* choose how to quantize this */
+            /* convert integer to string and assign to text */
+            yield return null;
+        }
+        //changeShopPhaseStatus(myPLayerID, false);
+        setShopPhase(false);
+        StopCoroutine(shopPhase(shopPhaseDuration));
+    }
+
+    [Command]
+    private void setShopPhase(bool newStatus)
+    {
+        isShopPhase = newStatus;
     }
     void OnBoardUpdated(SyncList<Unite>.Operation op, int index, Unite oldUnite, Unite newUnite)
     {
@@ -85,5 +175,6 @@ public class PlayerController : NetworkBehaviour
     {
         board.RemoveAt(unitePlacement);
     }
+
 
 }
